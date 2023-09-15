a9f11fa813195c1a4e69e69123062e6d51178136
Liseidy Bueno <liseidybueno@gmail.com>
2023-09-14 23:05:55 -0400

    add airbnb api from rapidapi

--- backend/index.js
+++ backend/index.js
@@ -24,11 +24,11 @@ app.get("/", (req, res) => {
 
 app.post("/search", async (req, res) => {
   const data = req.body;
-  console.log("***did it work", data);
 
   const city = data.city.replace(/ /g, "+");
   const budget = data.budget;
 
+  //Getting data from booking.com
   const url = `https://www.booking.com/searchresults.en-gb.html?ss=${city}&dest_type=city&checkin=2023-10-13&checkout=2023-10-14&nflt=price%3DUSD-min-${budget}-1`;
 
   const response = await axios.get(url);
@@ -36,8 +36,9 @@ app.post("/search", async (req, res) => {
 
   const hotels = [];
 
-  $("[data-testid=property-card]").each((index, element) => {
+  $("[data-testid=property-card]").each((element) => {
     const listing = {
+      source: "Booking.com",
       name: $(element).find("[data-testid=title]").text(),
       price: $(element).find("[data-testid=price-and-discounted-price]").text(),
       url: $(element)
@@ -52,8 +53,46 @@ app.post("/search", async (req, res) => {
     };
     hotels.push(listing);
   });
+});
 
-  res.json({ results: hotels });
+app.get("/airbnb", async (req, res) => {
+  const options = {
+    method: "GET",
+    url: "https://airbnb13.p.rapidapi.com/search-location",
+    params: {
+      location: "Paris",
+      checkin: "2023-09-16",
+      checkout: "2023-09-17",
+      adults: "1",
+      children: "0",
+      infants: "0",
+      pets: "0",
+      page: "1",
+      currency: "USD",
+    },
+    headers: {
+      "X-RapidAPI-Key": "47bc00c936msh21813d19de3c7e0p12c834jsn644689ad087b",
+      "X-RapidAPI-Host": "airbnb13.p.rapidapi.com",
+    },
+  };
+
+  const listings = [];
+
+  const response = await axios.request(options);
+  const results = response.data.results;
+  results.forEach((item) => {
+    const listing = {
+      name: item.name,
+      url: item.url,
+      img: item.hostThumbnail,
+      roomType: item.type,
+    };
+
+    listings.push(listing);
+  });
+  console.log("***listings", listings);
+
+  res.send(listings);
 });
 
 app.listen(PORT, () => {
